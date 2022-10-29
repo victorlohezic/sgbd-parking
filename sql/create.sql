0c52a9c0cbd1806acec77585f11949e3cf9843e9
SOULIE Alexandre <Alexandre.Soulie@bordeaux-inp.fr>
2022-10-29 20:25:01 +0200

    Add Parking table in SQL base

--- sql/create.sql
+++ sql/create.sql
@@ -6,3 +6,15 @@ create table COMMUNE (
 	NOM VARCHAR (50) NOT NULL,
 	CODE_POSTAL INT   NOT NULL
 );
+
+-- ============================================================
+--   Table : PARKING                                         
+-- ============================================================
+create table PARKING
+(
+	ID_PARKING				INT					not null,
+	NOM_PARKING				VARCHAR(50)					,
+	ADRESSE					VARCHAR(90)			not null,
+	TARIF_HORAIRE			INT					not null,
+	constraint pk_parking primary key (ID_PARKING)
+);

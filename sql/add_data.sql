1ea62f66fedeb4a07058560eeda5d4eb8fd37723
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2022-11-04 19:14:55 +0100

    add vehicules

--- sql/add_data.sql
+++ sql/add_data.sql
@@ -1,1 +1,25 @@
+-- ============================================================
+--    creation des donnees
+-- ============================================================
+
+-- COMMUNE
+
 insert into COMMUNE values (1, 'Lorient', '56100');
+insert into COMMUNE values (2, 'Bordeaux', '33000');
+insert into COMMUNE values (3, 'Talence', '33400');
+insert into COMMUNE values (4, 'Pau', '64000');
+insert into COMMUNE values (5, 'Nimes', '33000');
+
+
+-- VEHICULE 
+
+insert into VEHICULE values (1, 'Peugeot', '2014-06-09', 20000, 'bon');
+insert into VEHICULE values (2, 'Alpha Romeo', '2015-10-12', 45799, 'bon');
+insert into VEHICULE values (3, 'Fiat', '2016-11-15', 20673, 'bon');
+insert into VEHICULE values (4, 'Peugeot', '2014-06-25', 30000, 'bon');
+insert into VEHICULE values (5, 'Peugeot', '2021-03-16', 200, 'presque neuf');
+insert into VEHICULE values (6, 'Ford', '2020-06-12', 500, 'preque neuf');
+insert into VEHICULE values (7, 'Ford', '2001-02-18', 100000, 'mauvais');
+insert into VEHICULE values (8, 'Fiat', '2001-12-26', 50000, 'mauvais');
+insert into VEHICULE values (9, 'Peugeot', '1999-07-05', 80000, 'mauvais');
+insert into VEHICULE values (10, 'Fiat', '2007-06-21', 90000, 'mauvais');

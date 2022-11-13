b6696681a61eab971353e45a9ad7964b9472bbab
THIVIN NATHAN <nthivin@bordeaux-inp.fr>
2022-11-13 19:48:04 +0100

    ajout enregistrements

--- sql/add_data.sql
+++ sql/add_data.sql
@@ -6,7 +6,7 @@
 
 insert into COMMUNE values (1, 'Lorient', '56100');
 insert into COMMUNE values (2, 'Bordeaux', '33000');
-insert into COMMUNE values (3, 'Pessac', '33318');
+insert into COMMUNE values (3, 'Pessac', '33600');
 insert into COMMUNE values (4, 'Talence', '33400');
 insert into COMMUNE values (5, 'Pau', '64000');
 insert into COMMUNE values (6, 'Nimes', '30000');
@@ -25,3 +25,45 @@ insert into VEHICULE values ('AO-365-HD', 'Ford', '2001-02-18', 100000, 'mauvais
 insert into VEHICULE values ('YK-966-AH', 'Fiat', '2001-12-26', 50000, 'mauvais');
 insert into VEHICULE values ('PD-158-JK', 'Peugeot', '1999-07-05', 250000, 'mauvais');
 insert into VEHICULE values ('ZT-756-PA', 'Fiat', '2007-06-21', 90000, 'mauvais');
+
+
+-- PARKING
+
+insert into PARKING values (1, 'Village 1', '12 Av. de Collegno', 22);
+insert into PARKING values (2, 'Village 2', '7 Av. Pey Berland', 5);
+insert into PARKING values (3, 'Village 3', '9 Esp. des Antilles', 22);
+
+
+-- PLACE
+
+insert into PLACE values (101, 1, 1);
+insert into PLACE values (102, 2, 1);
+insert into PLACE values (103, 3, 1);
+insert into PLACE values (104, 4, 1);
+insert into PLACE values (105, 5, 1);
+
+insert into PLACE values (201, 1, 2);
+insert into PLACE values (202, 2, 2);
+insert into PLACE values (203, 3, 2);
+insert into PLACE values (204, 4, 2);
+insert into PLACE values (205, 5, 2);
+
+insert into PLACE values (301, 1, 3);
+insert into PLACE values (302, 2, 3);
+insert into PLACE values (303, 3, 3);
+insert into PLACE values (304, 4, 3);
+insert into PLACE values (305, 5, 3);
+
+
+
+-- TICKET
+
+insert into TICKET values (1, '2021-03-07', '14:09:48','18:23:27', 'YK-966-AH',104);
+insert into TICKET values (2, '2020-11-11', '07:51:13','14:44:27', 'OL-758-JV',202);
+insert into TICKET values (3, '2022-08-02', '16:42:45','19:26:27', 'PC-967-DK',205);
+insert into TICKET values (4, '2020-12-30', '09:13:27','17:49:27', 'ZT-756-PA',301);
+insert into TICKET values (5, '2022-04-14', '08:56:56','12:18:27', 'YK-966-AH',103);
+insert into TICKET values (6, '2021-09-19', '12:32:38','15:31:27', 'AO-365-HD',104);
+
+
+

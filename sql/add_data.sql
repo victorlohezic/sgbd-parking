6a5292f29b4066621e109c3425a59fc0462671ae
Alecsou <alexandresouliegadg@gmail.com>
2022-11-15 22:36:10 +0100

    Update incorrect data

--- sql/add_data.sql
+++ sql/add_data.sql
@@ -8,13 +8,13 @@ insert into COMMUNE values (1, 'Lorient', '56100');
 insert into COMMUNE values (2, 'Bordeaux', '33000');
 insert into COMMUNE values (3, 'Talence', '33400');
 insert into COMMUNE values (4, 'Pau', '64000');
-insert into COMMUNE values (5, 'Nimes', '33000');
+insert into COMMUNE values (5, 'Nimes', '30000');
 
 
 -- VEHICULE 
 
 insert into VEHICULE values (1, 'Peugeot', '2014-06-09', 20000, 'bon');
-insert into VEHICULE values (2, 'Alpha Romeo', '2015-10-12', 45799, 'bon');
+insert into VEHICULE values (2, 'Alfa Romeo', '2015-10-12', 45799, 'bon');
 insert into VEHICULE values (3, 'Fiat', '2016-11-15', 20673, 'bon');
 insert into VEHICULE values (4, 'Peugeot', '2014-06-25', 30000, 'bon');
 insert into VEHICULE values (5, 'Peugeot', '2021-03-16', 200, 'presque neuf');

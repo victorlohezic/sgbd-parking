d03842703286926b5ca793d0c74dbf41cbfc851e
Alecsou <alexandresouliegadg@gmail.com>
2022-11-16 19:16:07 +0100

    Add PHP block to enhance error detection in website code

--- website/create.php
+++ website/create.php
@@ -1,4 +1,11 @@
 <html>
+ <?php // Bloc PHP qui permet de spécifier une erreur sur la page
+ // Intéressant notamment lors d'erreurs 500 peu précises
+ /*error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);*/
+
+ ?>
  <?php include("template/header.php"); ?>
  <body>
    <?php include("template/navbar.php"); ?>
